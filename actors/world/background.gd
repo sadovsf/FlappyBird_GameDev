fb45c8a06873087876d5d4b8a69e193f31a2aa3a
Filip Sádovský <sadovsf@gmail.com>
2025-02-18 19:50:56 +0100

    Added day night cycle for background

--- actors/world/background.gd
+++ actors/world/background.gd
@@ -9,6 +9,8 @@ extends Node2D
 			# $Top a $Base by pak byl null a hra by crashnula.
 			_apply_speed()
 
+var _is_day = true
+
 
 func _ready():
 	# Aplikuje speed nastavenou v editoru.
@@ -21,3 +23,31 @@ func _apply_speed():
 	@warning_ignore("integer_division")
 	$Top.autoscroll.x = speed / 2
 	$Base.autoscroll.x = speed
+
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("kill"):
+		body.kill()
+
+
+func set_day(is_day :bool):
+	self._is_day = is_day
+	var tween = create_tween().set_parallel()
+	if is_day:
+		tween.tween_property($Top/City, "modulate:a", 1, 2)
+		tween.tween_property($Top/CityNight, "modulate:a", 0, 2)
+		
+		tween.tween_property($Top/Clouds, "modulate:a", 1, 2)
+		tween.tween_property($Top/CloudsNight, "modulate:a", 0, 2)
+	else:
+		tween.tween_property($Top/City, "modulate:a", 0, 2)
+		tween.tween_property($Top/CityNight, "modulate:a", 1, 2)
+		
+		tween.tween_property($Top/Clouds, "modulate:a", 0, 2)
+		tween.tween_property($Top/CloudsNight, "modulate:a", 1, 2)
+	
+	tween.play()
+
+
+func _on_day_night_cycle_timeout():
+	self.set_day(! self._is_day)
